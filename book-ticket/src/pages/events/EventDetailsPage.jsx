b784c1abf64514cae324aa2a738bd7aa0d2c475a
Ali-mohamed10 <messi01100665515@gmail.com>
2026-08-01 02:37:35 +0300

    remove SeatMapLegend component and its import from EventDetailsPage

--- book-ticket/src/pages/events/EventDetailsPage.jsx
+++ book-ticket/src/pages/events/EventDetailsPage.jsx
@@ -15,7 +15,6 @@ import { useEventBySlug } from '../../hooks/useEvents';
 import { EventInfoPanel } from '../../components/event/EventInfoPanel';
 import { EventDetailsSkeleton } from '../../components/event/EventDetailsSkeleton';
 import { InteractiveSeatMap } from '../../components/seatmap/InteractiveSeatMap';
-import { SeatMapLegend } from '../../components/seatmap/SeatMapLegend';
 import { BookingSummaryCard } from '../../components/booking/BookingSummaryCard';
 import { CheckoutCard } from '../../components/booking/CheckoutCard';
 
@@ -110,7 +109,6 @@ export const EventDetailsPage = () => {
                 onTableSelect={handleTableSelect}
                 selectedTables={selectedTables}
               />
-              <SeatMapLegend />
             </div>
           ) : (
             <div className="flex items-center justify-center h-96 bg-secondary/10 rounded-lg border border-border text-muted-foreground">
